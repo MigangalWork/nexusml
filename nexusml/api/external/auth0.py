349e5b63f1cc4c2221b70d08d3a9e430f2b7014e
Miguel Angel Alvarez <miguelalvfer.work@gmail.com>
2024-10-16 13:14:04 +0200

    docs: add docstrings to auth0.py

--- nexusml/api/external/auth0.py
+++ nexusml/api/external/auth0.py
@@ -1,24 +1,35 @@
 import re
 import urllib.parse
-import uuid
 
 import requests
-from flask import json, g
+from flask import g
 from requests import Response
 import os
 
 from werkzeug.exceptions import BadRequest
 
-from nexusml.env import ENV_AUTH0_DOJO_ACTIONS_CLIENT_ID, ENV_OAUTH_CONNECTION_ID, ENV_AUTH0_CLIENT_ID, \
+from nexusml.env import ENV_AUTH0_CLIENT_ID, \
     ENV_AUTH0_CLIENT_SECRET
 from nexusml.env import ENV_AUTH0_DOMAIN
 
 
 class Auth0Manager:
+    """Manages interactions with Auth0 API for retrieving, updating, and deleting user data.
+
+    This class provides functionalities to authenticate with Auth0, manage API tokens, and handle user data through
+    Auth0 Management API, including retrieving user information based on Auth0 ID or email, updating user details, and
+    deleting users from Auth0.
+    """
     auth0_domain_url: str = f'https://{os.environ[ENV_AUTH0_DOMAIN]}'
     auth0_user_url: str = f'https://{os.environ[ENV_AUTH0_DOMAIN]}/api/v2/users'
 
     def __init__(self, auth0_token: str = "") -> None:
+        """
+        Initializes the Auth0Manager with an optional token for authorization.
+
+        Args:
+            auth0_token (str): The Auth0 API token for authorization. Defaults to an empty string.
+        """
         self._auth0_token: str = auth0_token
         self.authorization_headers: dict = {'Authorization': 'Bearer ' + auth0_token}
 
@@ -26,6 +37,12 @@ class Auth0Manager:
             self.set_header()
 
     def set_header(self) -> None:
+        """
+        Sets the authorization headers by retrieving a new Auth0 management API token.
+
+        This function is called when no token is provided during initialization, ensuring that the instance has valid
+        credentials to interact with Auth0 Management API.
+        """
         auth0_token: str = self._get_auth0_management_api_token()
         self.authorization_headers = {'Authorization': 'Bearer ' + auth0_token}
         self._auth0_token = auth0_token
@@ -59,35 +76,6 @@ class Auth0Manager:
 
         return access_token
 
-
-    def match_oauth_user_uuid_or_email(self, access_token: str, user_uuid_or_email: str) -> dict:
-        """
-
-        Args:
-            access_token (str):
-            user_uuid_or_email:
-
-        Returns:
-
-        """
-        account_data: dict
-
-        regex_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-        if re.fullmatch(regex_email, user_uuid_or_email):
-            url: str = f'{self.auth0_user_url}?q=email:{user_uuid_or_email}&search_engine=v3'
-        else:
-            url: str = f'{self.auth0_user_url}/{user_uuid_or_email}'
-
-        response: Response = requests.get(url, headers=self.authorization_headers)
-        if response.status_code != 200:
-            raise BadRequest(description=f'No oauth Account associated with email "{user_uuid_or_email}"')
-
-        account_data = response.json()
-        if account_data and isinstance(account_data, list):
-            account_data: dict = account_data[0]
-
-        return account_data
-
     def get_auth0_user_data(self, auth0_id_or_email: str) -> dict:
         """
         Matches an Auth0 ID or email to retrieve the associated user data.
@@ -96,7 +84,7 @@ class Auth0Manager:
         and sends a GET request to retrieve the user account data. If the identifier is an email, it searches by email;
         otherwise, it searches by Auth0 ID.
 
-        WARNING: If more than one user data is received in the response, only the first user data will be returned
+        WARNING: If more than one user data is received in the response, only the first user data will be returned.
 
         Args:
             auth0_id_or_email (str): The Auth0 ID or email to match.
@@ -144,6 +132,18 @@ class Auth0Manager:
         response.raise_for_status()
 
     def patch_auth0_user(self, updated_data: dict):
+        """
+        Updates user data in Auth0 based on the provided information.
+
+        This function constructs the URL using the current user's Auth0 ID (retrieved from the `g` object),
+        and sends a PATCH request to update the user details with the provided data.
+
+        Args:
+            updated_data (dict): The new data to update the user with.
+
+        Raises:
+            requests.HTTPError: If the PATCH request does not return a status code of 2XX.
+        """
         url = f'{self.auth0_user_url}/{g.user_auth0_id}'
         headers: dict = self.authorization_headers
         headers['content-type'] = 'application/json'
